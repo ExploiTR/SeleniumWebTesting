9c7c167a1991f90b90d5e6143cff401f0402b01b
Pratim Majumder <ExploiTR@users.noreply.github.com>
2022-04-14 03:33:12 +0530

    update

--- OpenQA TEST/Elements/Elements/WebTables.cs
+++ OpenQA TEST/Elements/Elements/WebTables.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -56,6 +57,45 @@ namespace Elements
                 Thread.Sleep(500);
 
                 driver.FindElement(By.Id("submit")).Click();
+
+                Thread.Sleep(1000);
+
+                ReadOnlyCollection<IWebElement> rows = driver.FindElements(By.ClassName("rt-tr-group"));
+                int totalEntryCount = 0;
+                foreach (IWebElement element in rows)
+                {
+                    string text = element.FindElement(By.ClassName("rt-td")).GetAttribute("innerText").Trim();
+                    if (text.Equals("") || text.Contains("span"))
+                    {
+                        break;
+                    }
+                    totalEntryCount++;
+                }
+
+                Thread.Sleep(500);
+
+                // driver.FindElement(By.Id("edit-record-1")).Click();
+                driver.FindElements(By.XPath("//span[@title='Edit']"))[0].Click();
+                Thread.Sleep(100);
+                driver.SwitchTo().ActiveElement();
+
+                driver.FindElement(By.Id("firstName")).SendKeys("EditedName");
+                driver.FindElement(By.Id("lastName")).SendKeys("Name");
+                driver.FindElement(By.Id("userEmail")).Clear();
+                driver.FindElement(By.Id("userEmail")).SendKeys("edited@name.exe");
+                driver.FindElement(By.Id("age")).SendKeys("72");
+                driver.FindElement(By.Id("salary")).SendKeys("900000");
+                driver.FindElement(By.Id("department")).SendKeys("Manual Tester");
+
+                Thread.Sleep(500);
+
+                driver.FindElement(By.Id("submit")).Click();
+
+                driver.SwitchTo().ActiveElement();
+
+                Thread.Sleep(250);
+
+                driver.FindElements(By.XPath("//span[@title='Delete']"))[0].Click();
 
                 Thread.Sleep(1000);
 
